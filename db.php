997eaeb76278aaf0dc491c5fa0cccb055cb0856d
Evan Mattson <me@aaemnnost.tv>
2018-06-17 14:21:38 +0300

    add plugin file headers to db.php

--- db.php
+++ db.php
@@ -1,6 +1,14 @@
 <?php
 /**
- * This file must be placed in the directory wp-content/db.php.
+ * Plugin Name: WP SQLite DB
+ * Description: SQLite database driver drop-in. (based on SQLite Integration by Kojima Toshiyasu)
+ * Author: Evan Mattson
+ * Author URI: https://aaemnnost.tv
+ * Plugin URI: https://github.com/aaemnnosttv/wp-sqlite-db
+ * Version: 1.0
+ * Requires PHP: 5.4
+ *
+ * This file must be placed in wp-content/db.php.
  * WordPress loads this file automatically.
  *
  * This project is based on the original work of Kojima Toshiyasu and his SQLite Integration plugin.
