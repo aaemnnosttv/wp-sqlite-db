d8d6dcbe069f909ce11986b555cd82de17bb916a
Evan Mattson <me@aaemnnost.tv>
2018-09-01 15:43:23 +0300

    formatting

--- src/db.php
+++ src/db.php
@@ -84,18 +84,18 @@ HTML
             'Your PHP installation appears not to have the right PDO drivers loaded. These are required for this version of WordPress and the type of database you have specified.');
     }
 
-    /*
- * Notice:
- * Your scripts have the permission to create directories or files on your server.
- * If you write in your wp-config.php like below, we take these definitions.
- * define('DB_DIR', '/full_path_to_the_database_directory/');
- * define('DB_FILE', 'database_file_name');
- */
+    /**
+     * Notice:
+     * Your scripts have the permission to create directories or files on your server.
+     * If you write in your wp-config.php like below, we take these definitions.
+     * define('DB_DIR', '/full_path_to_the_database_directory/');
+     * define('DB_FILE', 'database_file_name');
+     */
 
-    /*
- * FQDBDIR is a directory where the sqlite database file is placed.
- * If DB_DIR is defined, it is used as FQDBDIR.
- */
+    /**
+     * FQDBDIR is a directory where the sqlite database file is placed.
+     * If DB_DIR is defined, it is used as FQDBDIR.
+     */
     if (defined('DB_DIR')) {
         if (substr(DB_DIR, -1, 1) != '/') {
             define('FQDBDIR', DB_DIR . '/');
@@ -110,10 +110,10 @@ HTML
         }
     }
 
-    /*
- * FQDB is a database file name. If DB_FILE is defined, it is used
- * as FQDB.
- */
+    /**
+     * FQDB is a database file name. If DB_FILE is defined, it is used
+     * as FQDB.
+     */
     if (defined('DB_FILE')) {
         define('FQDB', FQDBDIR . DB_FILE);
     } else {
