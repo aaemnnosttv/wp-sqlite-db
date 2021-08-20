a0ffe3ebe2a0535316f572a59e0630ad6f05551b
Evan Mattson <me@aaemnnost.tv>
2021-08-20 19:37:10 +0300

    Update required PHP in drop-in to match composer

--- src/db.php
+++ src/db.php
@@ -6,7 +6,7 @@
  * Author URI: https://aaemnnost.tv
  * Plugin URI: https://github.com/aaemnnosttv/wp-sqlite-db
  * Version: 1.1.0
- * Requires PHP: 5.4
+ * Requires PHP: 5.6
  *
  * This file must be placed in wp-content/db.php.
  * WordPress loads this file automatically.
